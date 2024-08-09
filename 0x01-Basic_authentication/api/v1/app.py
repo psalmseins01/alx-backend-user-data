540e69ea87fcf7a3ddd7c87c4234c5b872115b7f
Anthony Gbobo <psalmseins@gmail.com>
2024-08-09 08:24:53 -0700

    Update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from typing import Literal
+from typing import Tuple
 import os
 
 
@@ -23,14 +23,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple[str, Literal[401]]:
+def unauthorized(error) -> Tuple[jsonify, int]:
     """ Not found handler
     """
     return jsonify({"error": "unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> tuple[str, Literal[403]]:
+def forbidden(error) -> Tuple[jsonify, int]:
     """ Not found handler
     """
     return jsonify({"error": "Forbidden"}), 403
