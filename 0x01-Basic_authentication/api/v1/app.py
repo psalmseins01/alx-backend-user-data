aae355289ca995acd8280beace73b094e9b9d0f1
Anthony Gbobo <psalmseins@gmail.com>
2024-08-09 08:18:37 -0700

    Update app module

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from typing import Literal
 import os
 
 
@@ -21,8 +22,21 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[str, Literal[401]]:
+    """ Not found handler
+    """
+    return jsonify({"error": "unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> tuple[str, Literal[403]]:
+    """ Not found handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
