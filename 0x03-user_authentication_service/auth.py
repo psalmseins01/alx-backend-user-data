c66344bf4d560ca8ffd28b7ddf5c2d7665327e4b
Anthony Gbobo <psalmseins@gmail.com>
2024-08-16 17:32:57 -0700

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,7 +31,6 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with the given email and password.
